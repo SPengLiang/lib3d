13525be99ccb79b0aff75359852d7e325f23b32c
SPengLiang <1243592003@qq.com>
2020-06-30 10:53:13 +0800

    v1.0

---
+++ project.py
@@ -0,0 +1,278 @@
+import os
+import numpy as np
+import cv2 as cv
+from tqdm import tqdm
+
+
+def roty(t):
+    ''' Rotation about the y-axis. '''
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[c, 0, s],
+                     [0, 1, 0],
+                     [-s, 0, c]])
+
+def compute_box_3d(xyz, hwl, ry):
+    ''' Takes an object and a projection matrix (P) and projects the 3d
+        bounding box into the image plane.
+        Returns:
+            corners_2d: (8,2) array in left image coord.
+            corners_3d: (8,3) array in in rect camera coord.
+    '''
+    # compute rotational matrix around yaw axis
+
+    R = roty(ry)
+
+    # 3d bounding box dimensions
+    l = hwl[2]
+    w = hwl[1]
+    h = hwl[0]
+
+    # 3d bounding box corners
+    x_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+    y_corners = [0, 0, 0, 0, -h, -h, -h, -h]
+    z_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+
+    # rotate and translate 3d bounding box
+    corners_3d = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+    # print corners_3d.shape
+    corners_3d[0, :] = corners_3d[0, :] + xyz[0]
+    corners_3d[1, :] = corners_3d[1, :] + xyz[1]
+    corners_3d[2, :] = corners_3d[2, :] + xyz[2]
+    # print 'cornsers_3d: ', corners_3d
+    # only draw 3d bounding box for objs in front of the camera
+    if np.any(corners_3d[2, :] < 0.1):
+        corners_2d = None
+        return corners_2d, np.transpose(corners_3d)
+
+    return np.transpose(corners_3d)
+
+
+def corner_3d(bbox3d_input):
+    '''
+
+    Args:
+        bbox3d_input: a single bbox list with coordinates. format:(6)  [h, w, l, x, y, z, Ry]
+
+    Returns:
+        8 corners of the 3d box. format: narray(8*3) [h, w, l, x, y, z, Ry]
+    '''
+    '''
+        vertex define
+
+       0  ____ 1 
+      4 /|___/|            / z(l)
+       / /  / / 5         /
+    3 /_/_2/ /           /----->x(w)
+      |/___|/            |
+    7     6              | y(h)
+    '''
+    bbox3d = bbox3d_input.copy()
+
+    R = roty(bbox3d[-1])
+
+    # # 3d bounding box dimensions
+    h = bbox3d[0]
+    w = bbox3d[1]
+    l = bbox3d[2]
+
+    # 3d bounding box corners
+    x_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+    y_corners = [0, 0, 0, 0, -h, -h, -h, -h]
+    z_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+
+    # rotate and translate 3d bounding box
+    corners_3d_shift = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+    corners_3d = np.transpose(corners_3d_shift) + np.array([bbox3d[3], bbox3d[4], bbox3d[5]])
+
+    return corners_3d
+
+
+def convert_to_3d(depth, P2, upsample_factor, x_start, y_start):
+    '''
+    :param depth: depth map of current frame cropped area. SHAPE: A*B
+    :param P2: projection matrix of left RGB camera.  SHAPE: 4*3
+    :param upsample_factor: upsample factor of the cropped area.
+    :param x_start: start coordinates in image coordinates of x.
+    :param y_start: start coordinates in image coordinates of y.
+    :return:
+            points: 3D coordinates in real world of cropped area.   SHAPE: N*3
+            uv_points: corresponding 2D coordinates in image coordinates of 3D points  SHAPE: N*2
+    '''
+    fx = P2[0][0] * upsample_factor
+    fy = P2[1][1] * upsample_factor
+    cx = P2[0][2] * upsample_factor
+    cy = P2[1][2] * upsample_factor
+    # print(fx, fy, cx, cy)
+
+    x_tile = np.array(range(depth.shape[1])).reshape(1, -1) + x_start
+    points_x = np.tile(x_tile, [depth.shape[0], 1])
+
+    y_tile = np.array(range(depth.shape[0])).reshape(-1, 1) + y_start
+    points_y = np.tile(y_tile, [1, depth.shape[1]])
+
+    points_x = points_x.reshape((-1, 1))
+    points_y = points_y.reshape((-1, 1))
+    depth = depth.reshape((-1, 1))
+
+    # -------mask-------
+    # mask = np.where(depth != np.inf, True, False)
+    # points_x = points_x[mask].reshape((-1, 1))
+    # points_y = points_y[mask].reshape((-1, 1))
+    # depth = depth[mask].reshape((-1, 1))
+
+    uv_points = np.concatenate([points_x, points_y], axis=1)
+
+    points_x = (points_x - cx) / fx
+    points_y = (points_y - cy) / fy
+
+    points_x *= depth
+    points_y *= depth
+
+    points = np.concatenate([points_x, points_y, depth], axis=1)
+
+    return points, uv_points
+
+def convert_to_2d(point_cloud, P2):
+    '''
+    :param point_cloud: 3D point_cloud. SHAPE: N*3
+    :param P2: projection matrix of left RGB camera.  SHAPE: 4*3
+    :return:
+            points_2d: 3D coordinates project to image plane.  SHAPE: N*3 [u, v, depth]
+    '''
+    P2_inv_coor = np.concatenate([P2, np.array([[0, 0, 0, 1]])], axis=0)
+    P2_inv = np.linalg.inv(P2_inv_coor)
+
+    point_cloud_coor = np.concatenate([point_cloud, np.ones((point_cloud.shape[0], 1))], axis=1)
+    points_2d = np.dot(P2_inv, point_cloud_coor.T).T
+
+    return points_2d
+
+def convert_kitti_lidar_to_depth(kitti_calib_path, kitti_lidar_path, img_h, img_w):
+    '''
+    Args:
+        kitti_calib_path: current kitti image calib file path
+        kitti_lidar_path: current kitti lidar file path
+        img_h: current image height
+        img_w: current image width
+
+    Returns:
+        depth: lidar convert to depth image
+    '''
+
+    lidar_data = np.fromfile(str(kitti_lidar_path), dtype=np.float32, count=-1).reshape([-1,4])
+    lidar_data = np.concatenate([lidar_data[:, :3], np.ones((lidar_data.shape[0], 1))], axis=1)
+
+    calib = parse_kitti_calib(kitti_calib_path)
+    P2 = calib['P2']
+    R0_rect = np.eye(4, dtype= np.float32)
+    Tr_velo_to_cam = np.eye(4, dtype= np.float32)
+
+    R0_rect[:3, :3] = calib['R0_rect']
+    Tr_velo_to_cam[:3, :4] = calib['Tr_velo_to_cam']
+
+    trans_mat = np.dot(np.dot(P2, R0_rect), Tr_velo_to_cam)
+    trans_2d = np.dot(trans_mat, lidar_data.T).T
+    trans_2d[:, [0, 1]] /= trans_2d[:, [2]]
+
+    trans_2d = trans_2d[(trans_2d[:, 0] > 0) & (trans_2d[:, 0] < img_w) &
+                        (trans_2d[:, 1] > 0) & (trans_2d[:, 1] < img_h) &
+                        (trans_2d[:, 2] > 0)]
+
+    depth = np.zeros((img_h, img_w))
+    depth[trans_2d[:, 1].astype(np.int32), trans_2d[:, 0].astype(np.int32)] = trans_2d[:, 2]
+
+    return depth
+
+
+def rot_pc(f_angle, pc):
+    '''
+    Args:
+        f_angle: rotated angle (axis y)
+        pc: 3D coordinates in real world of cropped area.  SHAPE: N*3
+
+    Returns:
+        rotated 3d points.
+
+    '''
+    R = roty(f_angle)
+    r_p = np.dot(R, pc.T).T
+
+    return r_p
+
+
+def parse_kitti_calib(calib_path):
+    with open(calib_path, encoding='utf-8') as f:
+        text = f.readlines()
+        P0 = np.array(text[0].split(' ')[1:], dtype=np.float32).reshape(3, 4)
+        P1 = np.array(text[1].split(' ')[1:], dtype=np.float32).reshape(3, 4)
+        P2 = np.array(text[2].split(' ')[1:], dtype=np.float32).reshape(3, 4)
+        P3 = np.array(text[3].split(' ')[1:], dtype=np.float32).reshape(3, 4)
+
+        R0_rect = np.array(text[4].split(' ')[1:], dtype=np.float32).reshape(3, 3)
+        Tr_velo_to_cam = np.array(text[5].split(' ')[1:], dtype=np.float32).reshape(3, 4)
+        Tr_imu_to_velo = np.array(text[6].split(' ')[1:], dtype=np.float32).reshape(3, 4)
+        calib = {
+            'P0': P0,
+            'P1': P1,
+            'P2': P2,
+            'P3': P3,
+            'R0_rect': R0_rect,
+            'Tr_velo_to_cam': Tr_velo_to_cam,
+            'Tr_imu_to_velo': Tr_imu_to_velo,
+        }
+    return calib
+
+def convert_color_map(img):
+    temp = img / np.max(img) * 255
+    temp = temp.astype(np.uint8)
+    im_color = cv.applyColorMap(temp, cv.COLORMAP_JET)
+    return im_color
+
+
+# img = cv.imread('/private/pengliang/VNL/depth/000000.png', -1) / 256.
+# img = 384 / img
+# cv.imwrite('d3.png', convert_color_map(img))
+
+if __name__ == '__main__':
+    lidar_path = '/private/pengliang/KITTI3D/training/velodyne/'
+    calib_path = '/private/pengliang/KITTI3D/training/calib/'
+    left_path = '/private/pengliang/KITTI3D/training/image_2/'
+    lidar_depth_path = '/private/pengliang/KITTI3D/training/lidar_depth/'
+
+    all_files = sorted(os.listdir(lidar_path))
+
+    for f in tqdm(all_files):
+        img = cv.imread(os.path.join(left_path, f[:-4]+'.png'))
+        depth_img = convert_kitti_lidar_to_depth(calib_path+f[:-4]+'.txt',
+                                                 lidar_path+f,
+                                                 img.shape[0],
+                                                 img.shape[1])
+        cv.imwrite(os.path.join(lidar_depth_path, f[:-4]+'.png'), (depth_img*256).astype(np.uint16))
+
+    # lidar_path = '/private/pengliang/KITTI3D/training/velodyne/000003.bin'
+    # calib_path = '/private/pengliang/KITTI3D/training/calib/000003.txt'
+    #
+    # lidar_data = np.fromfile(str(lidar_path), dtype=np.float32, count=-1).reshape([-1,4])
+    # lidar_data = np.concatenate([lidar_data[:, :3], np.ones((lidar_data.shape[0], 1))], axis=1)
+    #
+    # calib = parse_kitti_calib(calib_path)
+    # P2 = calib['P2']
+    # R0_rect = np.eye(4, dtype= np.float32)
+    # Tr_velo_to_cam = np.eye(4, dtype= np.float32)
+    #
+    # R0_rect[:3, :3] = calib['R0_rect']
+    # Tr_velo_to_cam[:3, :4] = calib['Tr_velo_to_cam']
+    #
+    # trans_mat = np.dot(np.dot(P2, R0_rect), Tr_velo_to_cam)
+    # trans_2d = np.dot(trans_mat, lidar_data.T).T
+    # trans_2d[:, [0, 1]] /= trans_2d[:, [2]]
+    #
+    # trans_2d = trans_2d[(trans_2d[:, 0] > 0) & (trans_2d[:, 0] < 1275) &
+    #                     (trans_2d[:, 1] > 0) & (trans_2d[:, 1] < 374)]
+    #
+    # img = np.zeros((374, 1275))
+    # img[trans_2d[:, 1].astype(np.int32), trans_2d[:, 0].astype(np.int32)] = trans_2d[:, 2]
+    # disp = np.zeros_like(img)
+    # disp[img > 0] = 384 / img[img > 0]
+    # cv.imwrite('d3.png', convert_color_map(disp))
---
+++ utils.py
@@ -0,0 +1,201 @@
+import os
+import numpy as np
+import cv2 as cv
+from scipy.spatial import ConvexHull
+
+def calc_iou(box1, box2):
+    '''
+
+    Args:
+        box1: box1 to be calculated 2d iou. format:(4)  [x1, y1, x2, y2]
+        box2: box2 to be calculated 2d iou. format:(4)  [x1, y1, x2, y2]
+
+    Returns:
+        2d iou value of the box.
+    '''
+    area1 = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
+    area2 = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+
+    xx1 = np.maximum(box1[0], box2[0])
+    yy1 = np.maximum(box1[1], box2[1])
+    xx2 = np.minimum(box1[2], box2[2])
+    yy2 = np.minimum(box1[3], box2[3])
+
+    w = np.maximum(0.0, xx2 - xx1 + 1)
+    h = np.maximum(0.0, yy2 - yy1 + 1)
+    inter = w * h
+    ovr = inter / (area1 + area2 - inter)
+
+    return ovr
+
+
+def point_cloud_2_birdeye(points,
+                           res=0.1,
+                           side_range=(-50., 50.),  # left-most to right-most
+                           fwd_range=(0., 100.),  # back-most to forward-most
+                           ):
+    '''
+
+    Args:
+        points: 3D object points. format narray(N*3) [x, y, z]
+        res: resolution.
+        side_range: left-most to right-most (x range)
+        fwd_range: back-most to forward-most (z range)
+
+    Returns:
+        A bird-eye view map.
+    '''
+
+    x_points = points[:, 0]
+    y_points = points[:, 1]
+    z_points = points[:, 2]
+
+    # FILTER - To return only indices of points within desired cube
+    # Three filters for: Front-to-back, side-to-side, and height ranges
+    # Note left side is positive y axis in LIDAR coordinates
+    f_filt = np.logical_and((x_points > side_range[0]), (x_points < side_range[1]))
+    s_filt = np.logical_and((z_points > fwd_range[0]), (z_points < fwd_range[1]))
+    filter = np.logical_and(f_filt, s_filt)
+    indices = np.argwhere(filter).flatten()
+
+    # KEEPERS
+    x_points = x_points[indices]
+    y_points = y_points[indices]
+    z_points = z_points[indices]
+
+    # z_points[z_points > fwd_range[1]] = fwd_range[1]
+    # x_points[x_points < side_range[0]] = side_range[0]
+    # x_points[x_points > side_range[1]] = side_range[1]
+
+    # CONVERT TO PIXEL POSITION VALUES - Based on resolution
+    x_img = (x_points / res).astype(np.int32)  # x axis is -y in LIDAR
+    z_img = (z_points / res).astype(np.int32)  # y axis is -x in LIDAR
+
+
+    # SHIFT PIXELS TO HAVE MINIMUM BE (0,0)
+    # floor & ceil used to prevent anything being rounded to below 0 after shift
+    x_img -= int(np.floor(side_range[0] / res))
+
+    # z_img = (z_img.astype(np.float64) / res).astype(np.int32)
+
+    # CLIP HEIGHT VALUES - to between min and max heights
+    # pixel_values = np.clip(a=z_points,
+    #                        a_min=height_range[0],
+    #                        a_max=height_range[1])
+    #
+    # # RESCALE THE HEIGHT VALUES - to be between the range 0-255
+    # pixel_values = scale_to_255(pixel_values,
+    #                             min=height_range[0],
+    #                             max=height_range[1])
+
+    # INITIALIZE EMPTY ARRAY - of the dimensions we want
+    x_max = 1 + int((side_range[1] - side_range[0]) / res)
+    y_max = 1 + int((fwd_range[1] - fwd_range[0]) / res)
+    im = np.zeros([y_max, x_max], dtype=np.uint8)
+
+    # FILL PIXEL VALUES IN IMAGE ARRAY
+    im[-z_img, x_img] = 150
+
+    return im
+
+
+def poly_area(x,y):
+    """ Ref: http://stackoverflow.com/questions/24467972/calculate-area-of-polygon-given-x-y-coordinates """
+    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+
+
+def polygon_clip(subjectPolygon, clipPolygon):
+    """ Clip a polygon with another polygon.
+
+    Ref: https://rosettacode.org/wiki/Sutherland-Hodgman_polygon_clipping#Python
+
+    Args:
+      subjectPolygon: a list of (x,y) 2d points, any polygon.
+      clipPolygon: a list of (x,y) 2d points, has to be *convex*
+    Note:
+      **points have to be counter-clockwise ordered**
+
+    Return:
+      a list of (x,y) vertex point for the intersection polygon.
+    """
+
+    def inside(p):
+        return (cp2[0] - cp1[0]) * (p[1] - cp1[1]) > (cp2[1] - cp1[1]) * (p[0] - cp1[0])
+
+    def computeIntersection():
+        dc = [cp1[0] - cp2[0], cp1[1] - cp2[1]]
+        dp = [s[0] - e[0], s[1] - e[1]]
+        n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
+        n2 = s[0] * e[1] - s[1] * e[0]
+        n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
+        return [(n1 * dp[0] - n2 * dc[0]) * n3, (n1 * dp[1] - n2 * dc[1]) * n3]
+
+    outputList = subjectPolygon
+    cp1 = clipPolygon[-1]
+
+    for clipVertex in clipPolygon:
+        cp2 = clipVertex
+        inputList = outputList
+        outputList = []
+        s = inputList[-1]
+
+        for subjectVertex in inputList:
+            e = subjectVertex
+            if inside(e):
+                if not inside(s):
+                    outputList.append(computeIntersection())
+                outputList.append(e)
+            elif inside(s):
+                outputList.append(computeIntersection())
+            s = e
+        cp1 = cp2
+        if len(outputList) == 0:
+            return None
+    return (outputList)
+
+def convex_hull_intersection(p1, p2):
+    """ Compute area of two convex hull's intersection area.
+        p1,p2 are a list of (x,y) tuples of hull vertices.
+        return a list of (x,y) for the intersection and its volume
+    """
+    inter_p = polygon_clip(p1,p2)
+    if inter_p is not None:
+        hull_inter = ConvexHull(inter_p)
+        return inter_p, hull_inter.volume
+    else:
+        return None, 0.0
+
+def box3d_vol(corners):
+    ''' corners: (8,3) no assumption on axis direction '''
+    a = np.sqrt(np.sum((corners[0,:] - corners[1,:])**2))
+    b = np.sqrt(np.sum((corners[1,:] - corners[2,:])**2))
+    c = np.sqrt(np.sum((corners[0,:] - corners[4,:])**2))
+    return a*b*c
+
+
+def box3d_iou(corners1, corners2):
+    ''' Compute 3D bounding box IoU.
+
+    Input:
+        corners1: numpy array (8,3), assume up direction is negative Y
+        corners2: numpy array (8,3), assume up direction is negative Y
+    Output:
+        iou: 3D bounding box IoU
+        iou_2d: bird's eye view 2D bounding box IoU
+
+    todo (rqi): add more description on corner points' orders.
+    '''
+    # corner points are in counter clockwise order
+    rect1 = [(corners1[i,0], corners1[i,2]) for i in range(3,-1,-1)]
+    rect2 = [(corners2[i,0], corners2[i,2]) for i in range(3,-1,-1)]
+    area1 = poly_area(np.array(rect1)[:,0], np.array(rect1)[:,1])
+    area2 = poly_area(np.array(rect2)[:,0], np.array(rect2)[:,1])
+    inter, inter_area = convex_hull_intersection(rect1, rect2)
+    iou_2d = inter_area/(area1+area2-inter_area)
+    ymax = min(corners1[0,1], corners2[0,1])
+    ymin = max(corners1[4,1], corners2[4,1])
+    inter_vol = inter_area * max(0.0, ymax-ymin)
+    vol1 = box3d_vol(corners1)
+    vol2 = box3d_vol(corners2)
+    iou = inter_vol / (vol1 + vol2 - inter_vol)
+    return iou, iou_2d
